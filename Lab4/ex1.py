af78230d949b8780010dcec2e4624a955e2bf991
FILIMON Mihail <NE15503@dow.com>
2023-11-01 18:51:17 +0200

    Lab4 homework, need to do Ex3

---
+++ Lab4/ex1.py
@@ -0,0 +1,32 @@
+class WishStack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+
+stack = WishStack()
+stack.push("python")
+stack.push(31.05)
+stack.push(3)
+
+print(stack.pop())
+print(stack.pop())
+print(stack.peek())
